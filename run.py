6d66909ea33fe3cc1fbf6396f492e4296671ff4b
pip-install-python <pipinstallpython@gmail.com>
2024-05-21 12:33:44 -0500

    fixed a navbar issue on mobile

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ _dash_renderer._set_react_version("18.2.0")
 
 stylesheets = [
     "https://unpkg.com/@mantine/dates@7/styles.css",
-    "https://unpkg.com/@mantine/code-highlight@7/styles.css",
+    # "https://unpkg.com/@mantine/code-highlight@7/styles.css",
     "https://unpkg.com/@mantine/charts@7/styles.css",
     "https://unpkg.com/@mantine/carousel@7/styles.css",
     "https://unpkg.com/@mantine/notifications@7/styles.css",
